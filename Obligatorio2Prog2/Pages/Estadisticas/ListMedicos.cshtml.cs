7eb5d095235e480da175de7e9473d5c208607bc4
Felipe <Felipe@DESKTOP-76JUB27>
2025-12-02 03:05:31 -0300

    Pagina de estadisticas -Pagina de listado de pacientes -Pagina de listado de medicos -Correcion a la hora de reprogramar un turno

--- Obligatorio2Prog2/Pages/Estadisticas/ListMedicos.cshtml.cs
+++ Obligatorio2Prog2/Pages/Estadisticas/ListMedicos.cshtml.cs
@@ -1,12 +1,73 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Obligatorio2Prog2.Datos;
+using Obligatorio2Prog2.Modelos;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Obligatorio2Prog2.Pages.Estadisticas
 {
     public class ListMedicosModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _contexto;
+        public ListMedicosModel(ApplicationDbContext contexto)
         {
+            _contexto = contexto;
         }
+        public IEnumerable<Medico> Medicos { get; set; }
+
+        //Lista con todas las especialidades
+        public List<string> Especialidades { get; set; }
+
+        //Guarda en la propiedad Buscar el valor que venga en el formulario
+        [BindProperty(SupportsGet = true)]
+        public string Buscar { get; set; }
+
+        public async Task OnGet()
+        {
+            var MedicosDB = _contexto.Medicos.AsQueryable();
+            //Guarda en especialidades cada especialidad de los medicos sin repetir
+            Especialidades = _contexto.Medicos.Select(m => m.EspecialidadM).Distinct().ToList();
+
+            //Si hay un busqueda...
+            if (!string.IsNullOrWhiteSpace(Buscar))
+            {
+                //Quita los espacios vacios
+                Buscar = Buscar.ToLower().Replace(" ", "");
+
+                //Muestra solo los medicos que coincidan con la busqueda
+                MedicosDB = MedicosDB.Where(m => m.NombreM.ToLower().Replace(" ", "").Contains(Buscar)
+                || m.ApellidoM.ToLower().Replace(" ", "").Contains(Buscar)
+                || m.MatriculaM.Replace(" ", "").Contains(Buscar));
+            }
+
+            //Guarda los medicos para que se muestren.
+            Medicos = await MedicosDB.ToListAsync();
+        }
+
+
+        public async Task<IActionResult> OnPostOrdenar(string especialidad)
+        {
+            IQueryable<Medico> MedicosDB = _contexto.Medicos;
+
+
+            //OPCION PARA MOSTRAR TODOS LOS MEDICOS A LA HORA DE FILTRAR PERO PRIMERO LOS QUE TENGAN LA ESPECIALIDAD ELEGIDA
+            //MedicosDB = MedicosDB.OrderByDescending(m => m.EspecialidadM == especialidad);
+
+            //Si la opcion elegida no es Mostrar todos muestra solo los medicos que tengan de especialidad la elegida
+            if (especialidad != "todos")
+            {
+                MedicosDB = MedicosDB.Where(m => m.EspecialidadM == especialidad);
+            }
+
+            //Si no, los muestra todos
+            Medicos = await MedicosDB.ToListAsync();
+
+            //Vuelve a llenar el select con las especialidades
+            Especialidades = _contexto.Medicos.Select(m => m.EspecialidadM).Distinct().ToList();
+            return Page();
+        }
+
     }
 }
--- Obligatorio2Prog2/Pages/Estadisticas/ListPacientes.cshtml.cs
+++ Obligatorio2Prog2/Pages/Estadisticas/ListPacientes.cshtml.cs
@@ -1,12 +1,76 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Obligatorio2Prog2.Datos;
+using Obligatorio2Prog2.Modelos;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Obligatorio2Prog2.Pages.Estadisticas
 {
     public class ListPacientesModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _contexto;
+        public ListPacientesModel(ApplicationDbContext contexto)
         {
+            _contexto = contexto;
+        }
+        public IEnumerable<Paciente> Pacientes { get; set; }
+
+        //Guarda lo que busca el usuario en la propiedad Buscar
+        [BindProperty(SupportsGet = true)]
+        public string Buscar { get; set; }
+
+        public async Task OnGet()
+        {
+            var PacientesDB = _contexto.Pacientes.AsQueryable();
+
+            //Si hay una busqueda...
+            if (!string.IsNullOrWhiteSpace(Buscar))
+            {
+                //Quita los espacios vacios, guiones y puntos (guiones y puntos para la CI)
+                Buscar = Buscar.ToLower().Replace(".", "").Replace("-", "").Replace(" ", "");
+
+                //Muestra solo los pacientes que coincidan con la busqueda del usuario
+                PacientesDB = PacientesDB.Where(p => p.NombreP.ToLower().Replace(" ", "").Contains(Buscar)
+                || p.ApellidoP.ToLower().Replace(" ", "").Contains(Buscar)
+                || p.NumDocumentoP.Replace(" ", "").Replace(".", "").Replace("-", "").Contains(Buscar));
+            }
+
+            Pacientes = await PacientesDB.ToListAsync();
+        }
+
+
+        public async Task<IActionResult> OnPostOrdenar(string categoria)
+        {
+            //Prepara una consulta de pacientes que se puede filtrar varias veces antes de ejecutarse
+            IQueryable<Paciente> PacientesDB = _contexto.Pacientes;
+
+            //Dependiendo de la opcion elegida es como se filtrara la lista de pacientes.
+            if (categoria == "NombreP")
+            {
+                PacientesDB = PacientesDB.OrderBy(p => p.NombreP);
+            }
+
+            else if (categoria == "NombrePDesc")
+            {
+                PacientesDB = PacientesDB.OrderByDescending(p => p.NombreP);
+            }
+
+            else if (categoria == "ApellidoP")
+            {
+                PacientesDB = PacientesDB.OrderBy(p => p.ApellidoP);
+            }
+
+            else if (categoria == "ApellidoPDesc")
+            {
+                PacientesDB = PacientesDB.OrderByDescending(p => p.ApellidoP);
+            }
+
+            //Guarda en la lista de Pacientes la consulta filtrada
+            Pacientes = await PacientesDB.ToListAsync();
+
+            return Page();
         }
     }
 }
